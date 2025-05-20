dcd22680c7253df8e0a6716a5be0bee8a8d53f70
ravinder <ravibeniwal931@gmail.com>
2025-05-20 22:56:39 +0530

    trailing  zero

---
+++ introductory problems/Trailing_zero.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n ;
+    cin >> n ;
+
+    int k = log5(n)
+
+    for(int i =0 ; i < k ; i++){
+        int 
+    }
+
+    
+    return 0;
+}
